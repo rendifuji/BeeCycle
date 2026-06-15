2c79c8fcade962124062c0c05795c9f6e2c422ea
Torazon <nathanielgs1212@gmail.com>
2026-06-15 10:24:43 +0700

    sd

--- auth/logIn.php
+++ auth/logIn.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 $loginErrors = $_SESSION['login_errors'] ?? [];
 unset($_SESSION['login_errors']);
 $registerSuccess = $_SESSION['register_success'] ?? '';
--- auth/register.php
+++ auth/register.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 $registerErrors = $_SESSION['register_errors'] ?? [];
 unset($_SESSION['register_errors']);
 ?>
